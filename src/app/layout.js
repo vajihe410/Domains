5365f38e92ef4d3abae39d685ae0ef65c9839e10
vajihe <vajiheahmadi78@gmail.com>
2025-10-20 02:42:25 -0700

    Styles:delete google fonts and add global font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,10 @@
 //Styles
 import "./globals.css";
-//Fonts
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "Domains App",
-};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         {children}
       </body>
     </html>
